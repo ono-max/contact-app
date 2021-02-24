8488f90190e93fcdbc86c6204b29aac1c9491290
ono-max <nono19@students.desu.edu>
2021-02-24 19:01:37 +0900

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :questionnaires, only: [:create]
+    end
+  end
 end
